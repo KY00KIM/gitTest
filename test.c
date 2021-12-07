f1f2db65792da9d7e2831f6085b2e768525c70ef
KY00KIM <55953815+KY00KIM@users.noreply.github.com>
2021-12-07 18:25:26 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
+int sayHey (void);
+
 void main (void){
-  printf("hello");
+  
+  printf("hello\n");
+  
+  sayHey();
+  
+  return 0;
+}
+
+int sayHey(void){
+  printf("Hey~\n");
   return 0;
 }
   
