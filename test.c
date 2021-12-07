0d91634db4acfce5beebc6174b847604803a83b5
Kyumin-Kim <55953815+KY00KIM@users.noreply.github.com>
2021-12-07 18:50:59 +0900

    msg 2

--- test.c
+++ test.c
@@ -1,18 +1,28 @@
 #include<stdio.h>
 
 int sayHey (void);
+int printStr (char* s, int len){
+
 
 void main (void){
   
   printf("hello\n");
   
   sayHey();
+
+  char arr[100]= "";
+
+  printStr(arr, 10)
   
   return 0;
 }
 
-int sayHey(void){
+int sayHey (void) {
   printf("Hey~\n");
   return 0;
 }
   
+int printStr (char* s, int len){
+  printf("%s", s);
+  return 0;
+}
